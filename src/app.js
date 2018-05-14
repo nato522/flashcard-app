2e672a8d166c2ccc1add6b1cd00e15c092a53f9b
Renato PL <renapl90@gmail.com>
2018-05-14 18:40:54 +0200

    App and Sidebar moved to components folder

--- src/app.js
+++ src/app.js
@@ -1,53 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, combineReducers } from 'redux';
 import { addDeck, showAddDeck, hideAddDeck } from './actions';
 import * as reducers from './reducers';
+import App from './components/App';
+import Sidebar from './components/Sidebar';
 
 const ENTER_KEY = 13;
 
-const store = Redux.createStore(Redux.combineReducers(reducers));
+const store = createStore(combineReducers(reducers));
 
 store.subscribe(() => {
 	console.log(store.getState());
 });
-
-const App = (props) => {
-	return (
-		<div className="app">
-			{props.children}
-		</div>
-	);
-}
-
-class Sidebar extends React.Component {
-
-	componentDidUpdate(){
-		var el = ReactDOM.findDOMNode(this.refs.add);
-		if (el) el.focus();
-	}
-
-	render() {
-		let props = this.props;
-
-		return (
-			<div className="sidebar">
-				<h2>All Decks</h2>
-				<button onClick={ e => this.props.showAddDeck() }>New Deck</button>
-				<ul>
-					{props.decks.map((deck, i) =>
-						<li key={i}>{deck.name}</li>
-					)}
-				</ul>
-				{ props.addingDeck && <input ref='add' onKeyPress={this.createDeck.bind(this)} /> }
-			</div>
-		);
-	}
-
-	createDeck(evt){
-		if (evt.which !== ENTER_KEY) return;
-		var name = ReactDOM.findDOMNode(this.refs.add).value;
-		this.props.addDeck(name);
-		this.props.hideAddDeck();
-	}
-}
 
 function run(){
 	let state = store.getState();
